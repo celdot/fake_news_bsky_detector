e8b7cf55d4a1ba030fa7c0286e14826bf8d504cc
celdot <celine.dothuan@telecom-paris.fr>
2025-01-06 21:07:51 +0100

    server is working

--- server.py
+++ server.py
@@ -1,13 +1,46 @@
+import hopsworks
+import pandas as pd
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+import features_processing_utils as fpu
+from inference_pipeline import inference
+
 app = Flask(__name__)
 CORS(app)
+
 @app.route('/receive', methods=['POST'])
 def receive_input():
     data = request.get_json()
     user_input = data.get('input', '')
     print(f"Received input: {user_input}")
-    return jsonify({"status": "success", "received": user_input}), 200
+    
+    project = hopsworks.login()
+    fs = project.get_feature_store()
+    query_fg = fs.get_or_create_feature_group(
+        name='user_query',
+        description='Name of news article from user input',
+        version=1,
+        primary_key=['news_id'],
+        online_enabled=True,
+    )
+    try :
+        queries_df = query_fg.read()
+    except:
+        queries_df = pd.DataFrame()
+    
+    query_features = fpu.process_query(user_input)
+    query_features.columns = query_features.columns.str.replace(' ', '_')
+    
+    queries_df = pd.concat([queries_df, query_features], ignore_index=True)
+
+    query_fg.insert(queries_df, write_options={"wait_for_job": True})
+    
+    print("finished writing to feature store")
+    
+    result = inference(user_input)
+
+    return jsonify({"status": "success", "received": result}), 200 # Send result back as JSON
+
 if __name__ == '__main__':
     app.run(debug=True)
