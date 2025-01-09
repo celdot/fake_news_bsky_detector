757038b25f9aef219df34fe04f006d44f5740387
celdot <celine.dothuan@telecom-paris.fr>
2025-01-09 11:41:16 +0100

    trying to reduce computation time by removing pushing predictions to hopsworks

--- inference_pipeline.py
+++ inference_pipeline.py
@@ -20,9 +20,6 @@ def inference(input, project, fs):
     # Download the saved model files to a local directory
     saved_model_dir = retrieved_model.download()
     
-    # Initialize the model
-    model = RandomForestClassifier()
-    
     # Load the model from a saved JSON file
     model = joblib.load(os.path.join(saved_model_dir, "model.joblib"))
     
@@ -30,30 +27,9 @@ def inference(input, project, fs):
                     name="user_query",
                     version=1)
     
-    user_query = user_query_fg.select_all().read()
     features_to_predict = user_query_fg.select_except(["news_id"]).read()
-    
-    predictions_fg = fs.get_or_create_feature_group(
-    name="news_propagation_predictions",
-    version=1,
-    description="News propagation prediction results",
-    primary_key=["user_query"],
-    )
-    
-    try :
-        predictions_df = predictions_fg.read()
-    except:
-        predictions = model.predict(features_to_predict)
-        predictions_df = user_query["news_id"].to_frame().rename(columns={"news_id": "user_query"})
-        predictions_df["prediction"] = predictions
         
     prediction = model.predict(features_to_predict.tail(1))
-    new_prediction = pd.DataFrame({"user_query": user_query.tail(1)["news_id"].values[0], "prediction": prediction})
-    predictions_df = predictions_df._append(new_prediction, ignore_index=True)
-    
-    predictions_df["prediction"] = predictions_df["prediction"].astype('int64')
-    
-    predictions_fg.insert(predictions_df, write_options={"wait_for_job": True})
     
     if prediction == 0:
         return(f"The news {input} is real")
